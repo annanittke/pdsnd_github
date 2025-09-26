e9f5e844fb2e2593c01238b0357402fe8130c354
Nittke, Anna (T3-EH) <anna.nittke@cariad.technology>
2025-09-26 10:27:48 +0200

    refactor: improve readability in bikesharer_starter.py

--- bikeshare_starter.py
+++ bikeshare_starter.py
@@ -17,11 +17,7 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
-
     # get user input for month (all, january, february, ... , june)
-
-
     # get user input for day of week (all, monday, tuesday, ... sunday)
 
 
